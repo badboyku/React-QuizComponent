0e46735a08286904a3e52f44469674aa762c4410
badboyku <badboyku@gmail.com>
2020-06-07 00:48:17 -0700

    mod 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,8 +16,16 @@ class Quiz extends Component {
     });
   }
 
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
+
   render() {
-    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
+    const isQuizEnd = (
+      (
+        this.state.quiz_position - 1
+      ) === quizData.quiz_questions.length
+    );
 
     return (
       <div>
@@ -26,7 +34,8 @@ class Quiz extends Component {
             ? <QuizEnd/>
             : <QuizQuestion
               quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-              showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
+              showNextQuestionHandler={this.showNextQuestion.bind(this)}
+              resetClickHandler={this.handleResetClick.bind(this)}/>
         }
       </div>
     );
