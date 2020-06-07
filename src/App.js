26fc8a7ab958964dccead829bcdd86dbe3d1616b
badboyku <badboyku@gmail.com>
2020-06-06 23:12:57 -0700

    Step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import Quiz from './Quiz.js';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
-    return <Quiz/>;
+    return (
+      <div />
+    )
   }
 }
 
-export default App;
+export default App
