ada4183d95d2b6dfcb619fc925488ee55ec6d5a8
badboyku <badboyku@gmail.com>
2020-06-06 23:46:49 -0700

    mod 3 fix 1

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <li>
-        <button>{this.prop.button_text}</button>
+        <button>{this.props.button_text}</button>
       </li>
     );
   }
